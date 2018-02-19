d2bbabfd41e1ce707529b94d457212ed2c2aa44c
2B <hung.mighty.nguyen@gmail.com>
2018-02-20 00:04:11 +0700

    Compress uploading image based on its original size

--- PhotoTagger/Extension/UIImage+Extension.swift
+++ PhotoTagger/Extension/UIImage+Extension.swift
@@ -12,19 +12,42 @@ import UIKit
 extension UIImage {
   
   func compressBeforeUpload() -> Data? {
-    let imgCompressOptions: [CGFloat] = [0.9, 0.5, 0.2, 0.01]
-    for i in 0..<imgCompressOptions.count {
-      guard let imageData = UIImageJPEGRepresentation(self, imgCompressOptions[i]) else {
-        print("Could not get JPEG representation of UIImage")
-        return nil
-      }
-      
-      if imageData.getImageDataSizeInMB() < 4.5 {
-        return imageData
-      }
+    guard let originImageData = UIImageJPEGRepresentation(self, 1) else {
+      print("Could not get JPEG representation of UIImage")
+      return nil
     }
     
-    return nil
+    let originalSize = originImageData.getImageDataSizeInMB()
+    var imgCompressOption: CGFloat
+    
+    if originalSize > 500 {
+      imgCompressOption = 0.001
+    } else if originalSize > 400 {
+      imgCompressOption = 0.0015
+    } else if originalSize > 300 {
+      imgCompressOption = 0.003
+    } else if originalSize > 200 {
+      imgCompressOption = 0.007
+    } else if originalSize > 100 {
+      imgCompressOption = 0.01
+    } else if originalSize > 50 {
+      imgCompressOption = 0.03
+    } else if originalSize > 20 {
+      imgCompressOption = 0.05
+    } else if originalSize > 10 {
+      imgCompressOption = 0.2
+    } else if originalSize > 4 {
+      imgCompressOption = 0.8
+    } else {
+      imgCompressOption = 1
+    }
+    
+    guard let imageData = UIImageJPEGRepresentation(self, imgCompressOption) else {
+      print("Could not get JPEG representation of UIImage")
+      return nil
+    }
+    return imageData
+    
   }
   
 }
